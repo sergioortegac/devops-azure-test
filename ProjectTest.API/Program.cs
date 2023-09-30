793fe68852150ca2e5ea8dcf0526f66bbfb849ec
sergioortegac <sergioortegac@gmail.com>
2023-09-30 01:16:05 +0000

    Update Program.cs

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,7 +11,7 @@ var app = builder.Build();
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 7 Running ... 24092023-v1.0.0 CI/CD in AKS Pipelines con cambio en GH Test";
+    return "GET - Net 7 Running ... 24092023-v1.0.0 CI/CD in AKS Pipelines con cambio en GH Test Verdadero";
 });
 
 app.MapPost("/", () =>
